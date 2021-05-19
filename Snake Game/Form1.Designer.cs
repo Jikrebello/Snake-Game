48ca2f748485fb03a92e0ce81bdfacb955871625
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:49:48 +0200

    Add files via upload

---
+++ Snake Game/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace Snake_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Time = new System.Windows.Forms.Timer(this.components);
+            this.snakeBody1 = new System.Windows.Forms.PictureBox();
+            this.snakeBody2 = new System.Windows.Forms.PictureBox();
+            this.snakeHead = new System.Windows.Forms.PictureBox();
+            this.food = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.snakeBody1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snakeBody2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snakeHead)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Time
+            // 
+            this.Time.Interval = 800;
+            this.Time.Tick += new System.EventHandler(this.Time_Tick);
+            // 
+            // snakeBody1
+            // 
+            this.snakeBody1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.snakeBody1.Location = new System.Drawing.Point(110, 136);
+            this.snakeBody1.Name = "snakeBody1";
+            this.snakeBody1.Size = new System.Drawing.Size(20, 20);
+            this.snakeBody1.TabIndex = 1;
+            this.snakeBody1.TabStop = false;
+            // 
+            // snakeBody2
+            // 
+            this.snakeBody2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.snakeBody2.Location = new System.Drawing.Point(110, 160);
+            this.snakeBody2.Name = "snakeBody2";
+            this.snakeBody2.Size = new System.Drawing.Size(20, 20);
+            this.snakeBody2.TabIndex = 2;
+            this.snakeBody2.TabStop = false;
+            // 
+            // snakeHead
+            // 
+            this.snakeHead.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.snakeHead.Location = new System.Drawing.Point(110, 110);
+            this.snakeHead.Name = "snakeHead";
+            this.snakeHead.Size = new System.Drawing.Size(20, 20);
+            this.snakeHead.TabIndex = 0;
+            this.snakeHead.TabStop = false;
+            // 
+            // food
+            // 
+            this.food.BackColor = System.Drawing.Color.Gold;
+            this.food.Location = new System.Drawing.Point(0, 0);
+            this.food.Name = "food";
+            this.food.Size = new System.Drawing.Size(20, 20);
+            this.food.TabIndex = 3;
+            this.food.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 441);
+            this.Controls.Add(this.food);
+            this.Controls.Add(this.snakeBody2);
+            this.Controls.Add(this.snakeHead);
+            this.Controls.Add(this.snakeBody1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.snakeBody1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snakeBody2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.snakeHead)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.food)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer Time;
+        private System.Windows.Forms.PictureBox snakeBody1;
+        private System.Windows.Forms.PictureBox snakeBody2;
+        private System.Windows.Forms.PictureBox snakeHead;
+        private System.Windows.Forms.PictureBox food;
+    }
+}
+
---
+++ Snake Game/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Snake_Game
+{
+    public partial class Form1 : Form
+    {
+        const int MOVE_VALUE = 22;
+
+        int inputX = 0;
+        int inputY = -MOVE_VALUE; // starts it moving upwards
+
+        int x = 0;
+        int y = -MOVE_VALUE; // starts it moving upwards
+        int bodNum = 3;
+
+        int foodX = 0;
+        int foodY = 0;
+
+        int score = 0;
+
+        List<PictureBox> snake = new List<PictureBox>();
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            snake.Add(snakeHead);
+            snake.Add(snakeBody1);
+            snake.Add(snakeBody2);
+
+            GenerateFood();
+
+            Time.Start();
+        }
+
+        // Game loop that will tick every 1000 milliseconds (1 seconds)
+        private void Time_Tick(object sender, EventArgs e)
+        {
+            if (snakeHead.Location.X >= this.Width || snakeHead.Location.X <= 0 || snakeHead.Location.Y >= this.Height - 22 || snakeHead.Location.Y <= 0)
+            {
+                Time.Enabled = false;
+                MessageBox.Show("You lose. Your Score is: " + score);
+            }
+
+            x = inputX;
+            y = inputY;
+            SnakeMovement();
+            Collisions();
+            Eat();
+        }
+
+        // Movement
+        private void Form1_KeyDown(object sender, KeyEventArgs e) // gets keyboard input
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                if (y == MOVE_VALUE) // makes sure you can't go backwards
+                    return;
+
+                inputX = 0;
+                inputY = -MOVE_VALUE;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (y == -MOVE_VALUE)
+                    return;
+
+                inputX = 0;
+                inputY = MOVE_VALUE;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                if (x == -MOVE_VALUE)
+                    return;
+
+                inputX = MOVE_VALUE;
+                inputY = 0;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                if (x == MOVE_VALUE)
+                    return;
+
+                inputX = -MOVE_VALUE;
+                inputY = 0;
+            }
+        }
+        private void SnakeMovement()
+        {
+            for (int i = snake.Count - 1; i > 0; i--) // ensures that the rest of the body parts all follow the head
+            {
+                snake[i].Location = new Point(snake[i - 1].Location.X, snake[i - 1].Location.Y);
+            }
+
+            snakeHead.Location = new Point(snakeHead.Location.X + x, snakeHead.Location.Y + y);
+
+        }
+
+        // Adding to the Body
+        private void Eat()
+        {
+            if (food.Location == snakeHead.Location)
+            {
+                score++;
+                GrowSnake();
+                GenerateFood();
+            }
+        }
+        private void GrowSnake()
+        {
+            PictureBox snakeBody = new PictureBox()
+            {
+                Name = "snakeBody" + bodNum,
+                Size = new Size(20, 20),
+                BackColor = Color.DarkSeaGreen,
+                Location = new Point(1000, 1000)
+            };
+
+            bodNum++;
+            snake.Add(snakeBody);
+            Controls.Add(snakeBody);
+
+        }
+
+        // Collisions for snake and boundaries
+        private void Collisions()
+        {
+            bool isTouchingSnake;
+            do
+            {
+                int tempX = snakeHead.Left;
+                int tempY = snakeHead.Top;
+
+                isTouchingSnake = false;
+
+                for (int i = 1; i < snake.Count; i++)
+                {
+                    if (tempX == snake[i].Location.X && tempY == snake[i].Location.Y)
+                    {
+                        Time.Enabled = false;
+                        MessageBox.Show("You lose. Your Score is: " + score);
+                        break;
+                    }
+                }
+            } while (isTouchingSnake);
+        }
+
+        // Food
+        private void GenerateFood()
+        {
+            bool isOnSnake;
+            do
+            {
+                foodX = rand.Next(0, 20);
+                foodY = rand.Next(0, 20);
+                food.Left = foodX * MOVE_VALUE;
+                food.Top = foodY * MOVE_VALUE;
+
+                isOnSnake = false;
+
+                for (int i = 0; i < snake.Count; i++)
+                {
+                    if (food.Location.X == snake[i].Location.X && food.Location.Y == snake[i].Location.Y)
+                    {
+                        isOnSnake = true;
+                        break;
+                    }
+                }
+            } while (isOnSnake);
+        }
+
+    }
+}
